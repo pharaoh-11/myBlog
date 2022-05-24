ea8904d82e892693d00b999972a5772a078d4e04
vcukur <cukurika.vadim@yahoo.com>
2022-05-24 23:18:32 +0300

    Add express

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const http = require('http');
-const fs = require('fs');
-const homePage = fs.readFileSync('index.html');
-const aboutPage = fs.readFileSync('about.html');
-const contactPage = fs.readFileSync('contact.html');
-const notFoundPage = fs.readFileSync('notfound.html');
+const express = require('express');
+const path = require('path');
 
-const server = http.createServer((req, res) =>{
-    if(req.url === '/about')
-        res.end(aboutPage)
-    else if(req.url === '/contact')
-        res.end(contactPage)
-    else if(req.url === '/')
-        res.end(homePage)
-    else {
-        res.writeHead(404)
-        res.end(notFoundPage)
-    }
+const app = express();
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000")
 });
 
-server.listen(3000);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'index.html'));
+});
+
+app.get('/about',(req,res)=>{ // called when request to /about comes in
+    res.sendFile(path.resolve(__dirname,'about.html'))
+});
+
+app.get('/contact',(req,res)=>{ //called when request to /contact comes
+    res.sendFile(path.resolve(__dirname,'contact.html'))
+});
+
+
